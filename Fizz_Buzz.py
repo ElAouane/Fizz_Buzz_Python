6fa98a13d96c6a9fc6dfd617940c42e3a4db0ba8
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-07 10:59:53 +0000

    Implemented Fizz Buzz on a stand alone project

---
+++ Fizz_Buzz.py
@@ -0,0 +1,22 @@
+import time
+from Fizz_Buzz_Func import *
+while True:
+    count = 0
+    user = int(input("Choose a number: "))
+    while count < user:
+        count += 1
+        if fizz_buzz(count):
+            print('FizzBuzz')
+            continue
+        elif toc(count):
+            print('TOC')
+            continue
+        elif pop(count):
+            print('POP')
+            continue
+        print(count)
+        time.sleep(0.5)
+
+    ask = input("Would you like to keep playing?: Yes/No")
+    if ask.lower() == 'no':
+        break
